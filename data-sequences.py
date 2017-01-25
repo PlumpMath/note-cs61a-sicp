e0a4ac5a26f83cf7911c2e4d85d5cb2d060b8592
wu.fei@outlook.com <wu.fei@outlook.com>
2017-01-25 18:56:11 +0800

    added implementation of lists and dicts using nonlocal statements and dispatch fuctions

--- data-sequences.py
+++ data-sequences.py
@@ -136,3 +136,57 @@ def link_test():
     assert apply_to_all_link(lambda x: x * x, four) == [1, [4, [9, [16, 'empty']]]]
     assert keep_if_link(lambda x: x % 2 == 0, four) == [2, [4, 'empty']]
     assert join_link(four, ',') == '1, 2, 3, 4'
+
+
+# implementing lists with nonlocal state and dispatch functions
+def mutable_link():
+    """Return a functional implementation of a mutable linked list."""
+    contents = empty
+
+    def dispatch(message, value=None):
+        nonlocal contents
+        if message == 'len':
+            return len_link(contents)
+        elif message == 'getitem':
+            return getitem_link(contents, value)
+        elif message == 'push_first':
+            contents = link(value, contents)
+        elif message == 'pop_first':
+            f = first(contents)
+            contents = rest(contents)
+            return f
+        elif message == 'str':
+            return join_link(contents, ', ')
+    return dispatch
+
+
+def to_mutable_link(source):
+    """Return a functional list with the same contents as source."""
+    s = mutable_link()
+    for element in reversed(source):
+        s('push_first', element)
+    return s
+
+
+# implementing dictionaries with nonlocal state and dispatch functions
+def dictionary():
+    """Return a functional implementation of a dictionary."""
+    records = []
+
+    def getitem(key):
+        matches = [r for r in records if r[0] == key]
+        if len(matches) == 1:
+            key, value = matches[0]
+            return value
+
+    def setitem(key, value):
+        nonlocal records
+        non_matches = [r for r in records if r[0] != key]
+        records = non_matches + [[key, value]]
+
+    def dispatch(message, key=None, value=None):
+        if message == 'getitem':
+            return getitem(key)
+        elif message == 'setitem':
+            setitem(key, value)
+    return dispatch
